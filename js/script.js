879edfe91c16fcaedcd9de8e2fc64d3f4ffaa579
A P <131835703+s81c8f@users.noreply.github.com>
2024-04-30 20:18:24 -0400

    Add form input reader in JS

--- js/script.js
+++ js/script.js
@@ -21,3 +21,20 @@ Book.prototype.info = function() {
 function addBookToLibrary(book) {
   myLibrary.push(book);
 }
+
+// document.querySelector("#title");
+// document.querySelector("#author");
+// document.querySelector("#page-count");
+// document.querySelector("#has-read");
+
+const bookForm = document.querySelector("#book-form");
+bookForm.addEventListener("submit", function(event) {
+  event.preventDefault();
+  const formData = {
+    title: bookForm.title.value,
+    author: bookForm.author.value,
+    pageCount: bookForm["page-count"].value,
+    hasRead: bookForm["has-read"].checked
+  }
+  console.log(formData);
+})
