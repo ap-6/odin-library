9c2fbf2b1275ed7d6a397d96e72e6f68d0831ccb
A P <131835703+s81c8f@users.noreply.github.com>
2024-05-01 13:52:32 -0400

    Sync myLibrary array with DOM book list

--- js/script.js
+++ js/script.js
@@ -1,34 +1,15 @@
 function Book(title, author, pageCount, hasRead) {
+  Book.nextId = (Book.nextId || 0) + 1;
+
   this.title = title;
   this.author = author;
   this.pageCount = pageCount;
   this.hasRead = hasRead;
+  this.onDisplay = false;
+  this.id = Book.nextId;
 }
 
-
-
-const bookForm = document.querySelector("#book-form");
-bookForm.addEventListener("submit", function(event) {
-  event.preventDefault();
-
-  const formData = new Book(
-    bookForm.title.value, 
-    bookForm.author.value, 
-    bookForm["page-count"].value, 
-    bookForm["has-read"].checked
-  );
-  
-  //reset form
-  bookForm.title.value = "";
-  bookForm.author.value = "";
-  bookForm["page-count"].value = ""; 
-  bookForm["has-read"].checked = false;
-
-  createBookCard(formData);
-})
-
-function createBookCard(book) {
-  console.log("test");
+function createBookCard(book, myLibrary) {
   //create elements
   const bookList = document.querySelector("#book-list");
   const bookEntry = document.createElement("ul");
@@ -40,11 +21,19 @@ function createBookCard(book) {
   //style elements
   bookEntryContainer.classList.add("book");
   removeBtn.textContent = "Remove";
-  removeBtn.addEventListener("click", function(event) {
+  removeBtn.addEventListener("click", function() {
     bookEntryContainer.remove();
+    for (let  i = 0; i < myLibrary.length; i++) {
+      if (book.id === myLibrary[i].id) {
+        myLibrary.splice(i, 1);
+        break;
+      }
+    }
   })
 
   for (const key in book) {
+    if (key === "onDisplay" || key === "id") continue;
+
     const listItem = document.createElement("li");
     if (key === "hasRead") {
       listItem.textContent = book[key] == true ? "Has read" : "Has not read";
@@ -57,15 +46,45 @@ function createBookCard(book) {
 
 function runPage() {
   const myLibrary = [];
-  const bookList = document.querySelector("#book-list");
+
   //inserts generic book entry to start list
   const genericBook = new Book("Tao te Ching", "Lao Tsu", "110", true);
   addBookToLibrary(myLibrary, genericBook);
-  createBookCard(genericBook);
+  displayLibrary(myLibrary);
+
+  const bookForm = document.querySelector("#book-form");
+
+  bookForm.addEventListener("submit", function(event) {
+    event.preventDefault();
+
+    const formData = new Book(
+      bookForm.title.value, 
+      bookForm.author.value, 
+      bookForm["page-count"].value, 
+      bookForm["has-read"].checked
+    );
+    
+    //reset form
+    bookForm.title.value = "";
+    bookForm.author.value = "";
+    bookForm["page-count"].value = ""; 
+    bookForm["has-read"].checked = false;
+
+    addBookToLibrary(myLibrary, formData);
+    displayLibrary(myLibrary);
+  })
 }
 
 function addBookToLibrary(myLibrary, book) {
   myLibrary.push(book);
+}
+
+function displayLibrary(myLibrary) {
+  for(const book in myLibrary) {
+    if (myLibrary[book].onDisplay === true) continue;
+    createBookCard(myLibrary[book], myLibrary);
+    myLibrary[book].onDisplay = true;
+  }
 }
 
 runPage();
